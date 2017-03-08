faffbb75e0ad1514dc70b26bee58be70023c8338
Lakhezis <mina.skuld@protonmail.com>
2017-03-08 02:01:57 -0300

    Testeo de clase
    Pruebas para la clase Laberinto

---
+++ Pruebas/Laberinto.py
@@ -0,0 +1,7 @@
+class Laberinto:
+
+    def __init__(self):
+        print(3)
+
+    def inicializar(self):
+        print("Inicializa Laberinto")
---
+++ Pruebas/testeo.py
@@ -0,0 +1,30 @@
+import Laberinto
+
+##m= [[0,0], [0,0]]
+##m[0][1]=5
+#f=[0]*4
+#m=[f]*3
+
+#i=0
+#while i<=2:
+    #m[i]=["arriba","derecha","abajo","izquierda"]
+    #i=i+1
+
+
+#cadena=("bloqueado")
+#print(cadena)
+#m[0][3]=cadena
+#m[1][1]=cadena
+#m[2][2]=cadena
+
+#print(m)
+
+#class Laberinto:
+    #def inicializar(self):
+        #print("Inicializa Laberinto")
+
+
+lab=Laberinto.Laberinto()
+lab.inicializar()
+
+
