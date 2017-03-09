207d0c5c1e152a0c1c4f5efe16264b7cdbda5ef3
Lakhezis <mina.skuld@protonmail.com>
2017-03-08 21:22:51 -0300

    Se suben clases Hormiga y Laberinto
    +Se creó la matriz en Laberinto
    +Se creó la forma del laberinto
    +Se crearon métodos: Constructor y MemorizaCamino de la clase Hormiga

---
+++ Alpha 1.0/Hormiga.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+class Hormiga:
+
+    def __init__(self,nombre,ciclo):
+        self.__nombre=nombre
+        self.__ciclo=ciclo
+        self.__memoria=list()
+
+    def __memorizaCamino(self,rumbo):
+        __memoria.append(rumbo)
+
+    def
+
+
---
+++ Alpha 1.0/Main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import Laberinto
+
+lab=Laberinto.Laberinto()
+lab.imprimir()
+
+
+print(lab.getSalida())
+
+
